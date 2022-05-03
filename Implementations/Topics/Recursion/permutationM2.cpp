68e0d810132582194099702ca71e07fa56643111
kwai369 <97751891+kwai369@users.noreply.github.com>
2022-05-04 00:44:13 +0530

    Create permutationM2.cpp

---
+++ Implementations/Topics/Recursion/permutationM2.cpp
@@ -0,0 +1,12 @@
+/**
+  * Description : Generating permutation in another way.
+  * Source : USACO guide.
+  */
+
+vector<int> permutation;
+for (int i = 0; i < n; i++){
+    permutation.push_back(i);
+}
+do {
+// process permutation
+} while (next_permutation(permutation.begin(),permutation.end()));
