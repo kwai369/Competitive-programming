bc48259f2c8f1fb6226620e9f05b3f4f474ee6a2
kwai369 <97751891+kwai369@users.noreply.github.com>
2022-05-04 15:22:53 +0530

    Create prime_factorization.cpp

---
+++ Implementations/Topics/Number theory/Primes/prime_factorization.cpp
@@ -0,0 +1,18 @@
+/**
+  * Description : This program gives the prime factorization of a number n.
+  * Source : USACO guide.
+  */
+
+vector<int> factors(int n) {
+     vector<int> f;
+     for(int x = 2; x*x <= n; x++){
+        while (n%x == 0) {
+        f.push_back(x);
+        n /= x;
+        }
+     }  
+     if (n > 1) {
+        f.push_back(n);
+     }
+     return f;
+}
