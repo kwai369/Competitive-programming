a76b876caf1c19eabe2d4f7d734af9312b127cfe
kwai369 <97751891+kwai369@users.noreply.github.com>
2022-05-05 15:05:08 +0530

    Create backtracking.cpp

---
+++ Implementations/Topics/Recursion/backtracking.cpp
@@ -0,0 +1,19 @@
+/**
+  * Description : Solving the n queens problem with backtracking.
+  * Source : USACO guide.
+  */
+
+void search(int y) {
+      if (y == n) {
+          count++;
+          return;
+      }
+      for (int x = 0; x < n; x++) {
+          if (column[x] || diag1[x+y] || diag2[x-y+n-1]){ 
+              continue;
+          }
+          column[x] = diag1[x+y] = diag2[x-y+n-1] = 1;
+          search(y+1);
+          column[x] = diag1[x+y] = diag2[x-y+n-1] = 0;
+     }
+}
