00a949c8ed6144f8f53548c6831b5f09534791bb
kwai369 <dhruvkalmekh369@gmail.com>
2025-12-18 11:16:18 +0530

    Create soldier_and_bananas.cpp
    Problem link: https://codeforces.com/problemset/problem/546/A

---
+++ General/Basic_Problems/soldier_and_bananas.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void setIO(string s) {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	freopen((s+".in").c_str(),"r",stdin);
+	freopen((s+".out").c_str(),"w",stdout);
+}
+
+int main(){
+    //setIO("teleport");
+    ll k, w, n, total, borrow;
+    cin >> k >> n >> w;
+    total = k * ((w * (w+1))/2);
+    borrow = total - n;
+    if(borrow < 0){
+        borrow = 0;
+    }
+    cout << borrow;
+}
