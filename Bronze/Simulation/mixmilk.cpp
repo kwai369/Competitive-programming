03c1e156d6ecaf8e713f8889632198fab0d7469c
kwai369 <dhruvkalmekh369@gmail.com>
2026-01-08 00:51:07 +0530

    Update mixmilk.cpp
    Problem link: https://usaco.org/index.php?page=viewproblem2&cpid=855

--- Bronze/Simulation/mixmilk.cpp
+++ Bronze/Simulation/mixmilk.cpp
@@ -9,18 +9,19 @@ void setIO(string s) {
 
 int main(){
 	setIO("mixmilk");
-	int n = 3,cyc = 100;
-	vector<int> cap(n);
-	vector<int> milk(n);
-	for(int i=0;i<n;i++){
-		cin >> cap[i] >> milk[i];
-	}
-	for(int i=0;i<cyc;i++){
-		int b1 = i%n;
-		int b2 = (i+1)%n;
-		int mix = min(milk[b1],cap[b2]-milk[b2]);
-		milk[b1] -= mix;
-		milk[b2] += mix;
-	}
-	cout << milk[0] << "\n" << milk[1] << "\n" << milk[2]; 
+	int c[3], m[3];
+    for(int i = 0; i < 3; i++){    
+        cin >> c[i] >> m[i];
+    }
+    for(int i = 0; i < 100; i++){
+        int a = i % 3;
+        int b = (i+1) % 3;
+        int amt = min(m[a], c[b] - m[b]);
+        m[a] -= amt;
+        m[b] += amt;
+    }
+    for(int i = 0; i < 3; i++){
+        cout << m[i] << "\n";
+    }
+	return 0;
 }
