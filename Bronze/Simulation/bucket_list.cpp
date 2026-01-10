d8d85b6c0894bd0a7eaba35af1aaa49a00ebc778
kwai369 <dhruvkalmekh369@gmail.com>
2026-01-11 00:55:31 +0530

    Update bucket_list.cpp
    Problem link: https://usaco.org/index.php?page=viewproblem2&cpid=856

--- Bronze/Simulation/bucket_list.cpp
+++ Bronze/Simulation/bucket_list.cpp
@@ -1,5 +1,8 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define ll long long
 using namespace std;
+
+int RANGE[1001];
 
 void setIO(string s) {
 	ios_base::sync_with_stdio(0); cin.tie(0);
@@ -8,20 +11,23 @@ void setIO(string s) {
 }
 
 int main(){
-	setIO("blist");
-	int n;
-	cin >> n;
-	vector<int> vec(1001);
-	for(int i=0;i<n;i++){
-		int s,t,b;
-		cin >> s >> t >> b;
-		vec[s] = b;
-		vec[t] = -b;
-	}
-	int ans = 0,cur = 0;
-	for(int i=1;i<1001;i++){
-		cur += vec[i];
-		ans = max(cur,ans);
-	}
-	cout << ans;
+    setIO("blist");
+    int n;
+    cin >> n;
+    int first = 9999, last = -9999;
+    for(int i = 0; i < n; i++){
+        int start, end, buckets;
+        cin >> start >> end >> buckets;
+        RANGE[start] += buckets;
+        RANGE[end] -= buckets;
+        first = min(first, start);
+        last = max(last, end);
+    }
+    int max_buckets = 0, count = 0;
+    for(int i = first; i <= last; i++){
+        count += RANGE[i];
+        max_buckets = max(max_buckets, count);
+    }
+    cout << max_buckets << endl;
+    return 0;
 }
