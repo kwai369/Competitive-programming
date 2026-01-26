9a2fb5b40d69f63242078d038c496fe8caf715b6
kwai369 <dhruvkalmekh369@gmail.com>
2026-01-27 00:51:21 +0530

    Update mowing_b.cpp
    Problem link: https://usaco.org/index.php?page=viewproblem2&cpid=593

--- Bronze/Simulation/mowing_b.cpp
+++ Bronze/Simulation/mowing_b.cpp
@@ -1,4 +1,5 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define ll long long
 using namespace std;
 
 void setIO(string s) {
@@ -7,61 +8,67 @@ void setIO(string s) {
 	freopen((s+".out").c_str(),"w",stdout);
 }
 
+const int xmax = 2001;
+const int ymax = 2001;
+int field[xmax][ymax];
+
 int main(){
-	setIO("mowing");
-	vector<vector<int>> vec(2000,vector<int>(2000,0));
+    setIO("mowing");
 	int n;
 	cin >> n;
-	int mini = 1e9,xaxis=1000,yaxis=1000,count=0;
-	for(int i=0;i<n;i++){
+	int mini = 1e9;
+    int xaxis = 1000, yaxis = 1000;
+    int time = 0;
+	for(int i = 0; i < n; i++){
 		char dir;
 		int dis;
 		cin >> dir >> dis;
-		if(dir=='N'){
-			for(int j=yaxis;j<yaxis+dis;j++){
-				count++;
-				if(vec[xaxis][j]){
-					mini = min(mini,count-vec[xaxis][j]);
+		if(dir == 'N'){
+			for(int j = yaxis; j < yaxis + dis; j++){
+				time++;
+				if(field[xaxis][j]){
+					mini = min(mini, time - field[xaxis][j]);
 				}
-				vec[xaxis][j] = count;
+				field[xaxis][j] = time;
 			}
 			yaxis += dis;
 		}
-		else if(dir=='S'){
-			for(int j=yaxis;j>yaxis-dis;j--){
-				count++;
-				if(vec[xaxis][j]){
-					mini = min(mini,count-vec[xaxis][j]);
+		else if(dir == 'S'){
+			for(int j = yaxis; j > yaxis - dis; j--){
+				time++;
+				if(field[xaxis][j]){
+					mini = min(mini, time - field[xaxis][j]);
 				}
-				vec[xaxis][j] = count;
+				field[xaxis][j] = time;
 			}
 			yaxis -= dis;
 		}
-		else if(dir=='E'){
-			for(int j=xaxis;j<xaxis+dis;j++){
-				count++;
-				if(vec[j][yaxis]){
-					mini = min(mini,count-vec[j][yaxis]);
+		else if(dir == 'E'){
+			for(int j = xaxis; j < xaxis + dis; j++){
+				time++;
+				if(field[j][yaxis]){
+					mini = min(mini, time - field[j][yaxis]);
 				}
-				vec[j][yaxis] = i;
+				field[j][yaxis] = time;
 			}
 			xaxis += dis;
 		}
 		else{
-			for(int j=xaxis;j>xaxis-dis;j--){
-				count++;
-				if(vec[j][yaxis]){
-					mini = min(mini,count-vec[j][yaxis]);
+			for(int j = xaxis; j > xaxis - dis; j--){
+				time++;
+				if(field[j][yaxis]){
+					mini = min(mini, time - field[j][yaxis]);
 				}
-				vec[j][yaxis] = i;
+				field[j][yaxis] = time;
 			}
 			xaxis -= dis;
 		}
 	}
-	if(mini==1e9){
-		cout << -1;
+	if(mini == 1e9){
+		cout << -1 << endl;
 	}
 	else{
-		cout << mini;
+		cout << mini << endl;
 	}
+    return 0;
 }
