1f8996ae585849f5005fc7720dc1af3bf8046fd4
kwai369 <dhruvkalmekh369@gmail.com>
2026-02-13 01:24:04 +0530

    Update max_dis.cpp
    Problem link: https://codeforces.com/gym/102951/problem/A

--- Bronze/B.C.S/max_dis.cpp
+++ Bronze/B.C.S/max_dis.cpp
@@ -1,28 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-void setIO(string s) {
-	ios_base::sync_with_stdio(0); cin.tie(0);
-	freopen((s+".in").c_str(),"r",stdin);
-	freopen((s+".out").c_str(),"w",stdout);
-}
-
+ 
 int main(){
 	int n;
 	cin >> n;
 	vector<int> x(n);
 	vector<int> y(n);
-	for(int i=0;i<n;i++){
+	for (int i = 0; i < n; ++i){
 		cin >> x[i];
 	}
-	for(int i=0;i<n;i++){
+	for (int i = 0; i < n; ++i){
 		cin >> y[i];
 	}
-	int maxi = 0; 
-	for(int i=0;i<n;i++){
-		for(int j=i+1;j<n;j++){
-			maxi = max(maxi,(x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]));
+	int ans = INT_MIN;
+	for (int i = 0; i < n; ++i){
+		for (int j = i; j < n; ++j){
+			int a = (x[j] - x[i]) * (x[j] - x[i]);
+			int b = (y[j] - y[i]) * (y[j] - y[i]);
+			ans = max(ans, a+b);
 		}
 	}
-	cout << maxi;
+	cout << ans;
 }
