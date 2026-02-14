ce4dd88c62baadad60b579c939859a37bbfa172e
kwai369 <dhruvkalmekh369@gmail.com>
2026-02-14 08:06:47 +0530

    Update dia_col.cpp
    Problem link: https://usaco.org/index.php?page=viewproblem2&cpid=639
    Note: Both methods 1 and 2 take the same amount of time, i.e. O(n^2).

--- Bronze/B.C.S/dia_col.cpp
+++ Bronze/B.C.S/dia_col.cpp
@@ -1,4 +1,5 @@
-#include <bits/stdc++.h>
+// Method 1: Simple sweeping
+/*#include <bits/stdc++.h>
 using namespace std;
 
 void setIO(string s) {
@@ -26,4 +27,41 @@ int main(){
 		ans = max(ans,count);
 	}
 	cout << ans;
+}*/
+
+// Method 2: Using sort
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void setIO(string s) {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	freopen((s+".in").c_str(),"r",stdin);
+	freopen((s+".out").c_str(),"w",stdout);
+}
+
+int main(){
+    setIO("diamond");
+    int n, k;
+	cin >> n >> k;
+	vector<int> sizes(n);
+	for(int i = 0; i < n; i++){
+		cin >> sizes[i];
+	}
+	sort(sizes.begin(), sizes.end());
+	int ans = 0;
+	for(int i = 0; i < n; i++){
+		int low = sizes[i], high = sizes[i];
+		int count = 0;
+		for(int j = 0; j < n; j++){
+			if(abs(low - sizes[j]) <= k && abs(high - sizes[j]) <= k){
+				count++;
+				low = min(low, sizes[j]);
+				high = max(high, sizes[j]);
+			}
+		}
+		ans = max(ans, count);
+	}
+	cout << ans << endl;
+	return 0;
 }
