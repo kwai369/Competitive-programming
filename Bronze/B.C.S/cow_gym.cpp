9ffed74bd9f615151d58b097cd52541d5b22850e
kwai369 <dhruvkalmekh369@gmail.com>
2026-02-17 19:49:49 +0530

    Update cow_gym.cpp
    Problem link: https://usaco.org/index.php?page=viewproblem2&cpid=963
    Note: Both methods 1 and 2 take the same amount of time, O(K*N^2).

--- Bronze/B.C.S/cow_gym.cpp
+++ Bronze/B.C.S/cow_gym.cpp
@@ -1,4 +1,6 @@
-#include <bits/stdc++.h>
+// Method 1: 
+/*#include<bits/stdc++.h>
+#define ll long long
 using namespace std;
 
 void setIO(string s) {
@@ -8,31 +10,72 @@ void setIO(string s) {
 }
 
 int main(){
-	setIO("gymnastics");
-	int k,n;
+    setIO("gymnastics");
+    int k, n;
 	cin >> k >> n;
-	vector<vector<int>> rank(n,vector<int>(k,0));
-	for(int i=0;i<k;i++){
-		for(int j=0;j<n;j++){
-			int c;
-			cin >> c;
-			c--;
-			rank[c][i] = j;
+	vector<vector<int>> rankings(n, vector<int>(k));
+	for(int i = 0; i < k; i++){
+		for(int j = 0; j < n; j++){
+			int a;
+			cin >> a;
+			rankings[a - 1][i] = j + 1;
 		}
 	}
-	int ans=0;
-	for(int i=0;i<n;i++){
-		for(int j=i+1;j<n;j++){
-			int valid=0;
-			for(int e=0;e<k;e++){
-				if(rank[j][e] < rank[i][e]){
-					valid++;
+	int count = 0;
+	for(int i = 0; i < n; i++){
+		for(int j = i + 1; j < n; j++){
+			int consistent = 0;
+			for(int l = 0; l < k; l++){
+				if(rankings[i][l] < rankings[j][l]){
+					consistent--;
+				}
+				if(rankings[i][l] > rankings[j][l]){
+					consistent++;
 				}
 			}
-			if(valid==0 || valid==k){
-				ans++;
+			if(consistent == -k || consistent == k){
+				count++;
 			}
 		}
 	}
-	cout << ans;
+	cout << count << "\n";
+	return 0;
+}*/
+
+// Method 2: 
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void setIO(string s) {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	freopen((s+".in").c_str(),"r",stdin);
+	freopen((s+".out").c_str(),"w",stdout);
+}
+
+int main(){
+    setIO("gymnastics");
+    int k, n;
+	cin >> k >> n;
+	vector<vector<bool>> ranking_matrix(n, vector<bool>(n));
+	for(int i = 0; i < k; i++){
+		vector<int> ranks(n);
+		for(int& x : ranks){
+			cin >> x;
+			x--;
+		}
+		for(int j = 0; j < n; j++){
+			for(int l = j + 1; l < n; l++){
+				ranking_matrix[ranks[j]][ranks[l]] = true;
+			}
+		}
+	}
+	int count = 0;
+	for(int i = 0; i < n; i++){
+		for(int j = i + 1; j < n; j++){
+			count += (!ranking_matrix[i][j] || !ranking_matrix[j][i]);
+		}
+	}
+	cout << count << "\n";
+	return 0;
 }
