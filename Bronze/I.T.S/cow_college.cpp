246792ddcbda0ec198410dee849ea8d74d0dbe3f
kwai369 <dhruvkalmekh369@gmail.com>
2026-03-01 19:04:25 +0530

    Update cow_college.cpp
    Problem link: https://usaco.org/index.php?page=viewproblem2&cpid=1251

--- Bronze/I.T.S/cow_college.cpp
+++ Bronze/I.T.S/cow_college.cpp
@@ -1,29 +1,25 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-
-bool comp(pair<ll,ll> a,pair<ll,ll> b){
-	if(a.first != b.first){
-		return a.first < b.first;
-	}
-	return a.second > b.second;
-}
+using ll = long long;
 
 int main(){
 	int n;
 	cin >> n;
-	vector<ll> tui(n+1);
-	for(int i=1;i<n+1;i++){
-		cin >> tui[i];
+	vector<int> vec(n);
+	for(auto& i : vec){
+		cin >> i;
 	}
-	sort(tui.begin()+1,tui.end());
-	vector<pair<ll,ll>> vec(n+1);
-	for(int i=1;i<n+1;i++){
-		int fea = (n-i) + 1;
-		vec[i].first = (tui[i] * fea);
-		vec[i].second = tui[i];
+	ll maxi = INT_MIN;
+	int mini = INT_MAX;
+	sort(vec.begin(), vec.end());
+	for(int i = 0; i < n; i++){
+		ll approx = ll(vec[i]) * ll((n - 1) - i + 1);
+		if(approx > maxi){
+			maxi = approx;
+			mini = vec[i];
+		}
 	}
-	sort(vec.begin(),vec.end(),comp);
-	cout << vec[n].first << " " << vec[n].second;
+	cout << maxi << " " << mini << endl;
+	return 0;
 }
