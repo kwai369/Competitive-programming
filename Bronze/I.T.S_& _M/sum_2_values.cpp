65e8c6275dc8a8220f59d285c7af3e7465ce736b
kwai369 <dhruvkalmekh369@gmail.com>
2026-03-06 03:56:16 +0530

    Update sum_2_values.cpp
    Problem link: https://cses.fi/problemset/task/1640/

--- Bronze/I.T.S_& _M/sum_2_values.cpp
+++ Bronze/I.T.S_& _M/sum_2_values.cpp
@@ -1,27 +1,19 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-
-int main(){
-	int n;
-	ll x;
-	cin >> n >> x;
-	vector<ll> vec(n);
-	for(int i=0;i<n;i++){
-		cin >> vec[i];
-	}
-	int ans = 0;
-	map<ll,ll> mp;
-	for(int i=0;i<n;i++){
-		if(mp.count(x - vec[i])){
-			ans++;
-			cout << i+1 << " " << mp[x - vec[i]];
-			break;
-		}
-		mp[vec[i]] = i+1;
-	}
-	if(ans == 0){
-		cout << "IMPOSSIBLE";
-	}
+int main() {
+	int n, x;
+    cin >> n >> x;
+    map<int, int> mp;
+    for (int i = 1; i <= n; i++) {
+        int a;
+        cin >> a;
+        if (mp[x - a]) {
+            cout << mp[x - a] << " " << i << "\n";
+            return 0;
+        }
+        mp[a] = i;
+    }
+    cout << "IMPOSSIBLE\n";
+	return 0;
 }
